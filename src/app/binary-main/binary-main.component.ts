0119a76547ddea0da65e27384386edf69b1444ec
kalivaraprasad gonapa <77320853+Streen9@users.noreply.github.com>
2023-06-18 19:15:14 +0530

    add comments to code

--- src/app/binary-main/binary-main.component.ts
+++ src/app/binary-main/binary-main.component.ts
@@ -6,7 +6,6 @@ import {
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
-// import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import {
   HttpClient,
@@ -14,18 +13,10 @@ import {
   HttpErrorResponse,
 } from '@angular/common/http';
 
-// const poppingAnimation = trigger('popping', [
-//   state('in', style({ opacity: 1, transform: 'scale(1)' })),
-//   transition(':enter', [
-//     style({ transform: 'scale(0)' }),
-//     animate('200ms ease-out', style({ transform: 'scale(1)' }))
-//   ]),
-// ]);
 @Component({
   selector: 'app-binary-main',
   templateUrl: './binary-main.component.html',
   styleUrls: ['./binary-main.component.scss'],
-  // animations: [poppingAnimation],
   encapsulation: ViewEncapsulation.None, //after rendring the tree need to apply styles again
 })
 export class BinaryMainComponent implements OnInit {
@@ -44,12 +35,20 @@ export class BinaryMainComponent implements OnInit {
   colorBack!: string;
   searchValue!: string;
 
+  /**
+   * ngOnInit()
+   * Angular lifecycle hook called after the component is initialized.
+   * It calls the `displayTree()` ` methods and retrieves data from an API using `getData()`.
+   */
   ngOnInit() {
     this.displayTree();
-    this.checkLevel();
     this.getData();
   }
-
+  /**
+   * getData()
+   * Retrieves the binary tree data from an API using the Angular `HttpClient` and assigns the response to the `root` property.
+   * If the `root` is `null`, it sets the `rootFlag` to `true`.
+   */
   getData() {
     const data = this.http
       .get<any>('https://bst-service.onrender.com/')
@@ -64,7 +63,12 @@ export class BinaryMainComponent implements OnInit {
         }
       });
   }
-
+  /**
+   * clearInput()
+   * Clears the input values and resets the binary tree.
+   * If the user confirms the action, it sets the `root` to `null`, `rootNode` to `null`, and `rootFlag` to `true`.
+   * It also calls the `displayTree()` and `postIntoJson()` methods.
+   */
   clearInput() {
     if (this.root) {
       if (confirm('Are you sure?')) {
@@ -79,21 +83,37 @@ export class BinaryMainComponent implements OnInit {
       alert('No Tree is Existed!');
     }
   }
+
+  /**
+   * displayTree()
+   * Renders and displays the binary tree in the DOM.
+   * It uses the `renderTree()` method to generate the HTML content and updates the `treeContainer` element.
+   */
   displayTree() {
     //to render tree
     const treeEl = this.treeContainer.nativeElement;
     if (treeEl) {
       treeEl.innerHTML = this.renderTree(this.root);
-      // this.bindDeleteEvent(treeEl);
     }
   }
 
+  /**
+   * searchElement()
+   * Searches for a given value in the binary tree and highlights the corresponding node in the DOM by adding a CSS class.
+   * It calls the `highlightTree()` method.
+   */
   searchElement() {
     this.highlightTree(this.searchValue);
   }
 
+  /**
+   * highlightTree(value: string)
+   * Highlights a node with the specified value by adding a CSS class.
+   * It selects the element using the value attribute and adds the `highlight` class.
+   * After a delay of 2 seconds, it removes the `highlight` class.
+   */
+
   highlightTree(value: string) {
-    // const value = "24";/
     const element = document.querySelector(
       '.nodeElement[nodevalue="' + value + '"]'
     );
@@ -103,18 +123,34 @@ export class BinaryMainComponent implements OnInit {
     }, 2000);
   }
 
+  /**HighMin()
+   * Highlight the Minimum Character in the tree
+   */
+
   highMin() {
     this.highlightTree(this.findMinValue(this.root));
   }
+  /**HighMax()
+   * Highlight the Maximum Character in the tree
+   */
   highMax() {
     this.highlightTree(this.findMaxValue(this.root));
   }
+
+  /**
+   * deleteElement(value: string)
+   * Deletes a node with the specified value from the binary tree.
+   * It calls the `deleteNode()` method and updates the tree by calling `displayTree()`.
+   */
   deleteElement(value: string): void {
     console.log('deleting');
     this.root = this.deleteNode(this.root, value);
     this.displayTree();
   }
 
+  /**removeElement()
+   * The removeElement method removes a node with the specified value from the binary tree.
+   * It calls the deleteNode method, updates the tree, and checks if the root is null to set the rootFlag accordingly. */
   removeElement() {
     if (this.root) {
       this.root = this.deleteNode(this.root, this.deleteValue);
@@ -128,8 +164,11 @@ export class BinaryMainComponent implements OnInit {
     }
   }
 
-  //adding the node into object with addNode function
-  onSubmit(): void {
+  /**onSubmit()
+   * The onSubmit method is triggered when a form is submitted to
+   * add a new node to the binary tree. It creates a new Node instance
+   * and adds it to the tree using the addNode method. */
+  onSubmit() {
     console.log(this.rootNode, 'nodes');
     const trimmedValue = this.rootNode.trim();
     if (/^-?\d+$/.test(trimmedValue)) {
@@ -150,7 +189,12 @@ export class BinaryMainComponent implements OnInit {
     }
   }
 
-  //delete the node
+  /**
+   * deleteNode(node: Node | null, value: string)
+   * Deletes a node with the specified value from the binary tree.
+   * It recursively traverses the tree to find the node to delete and performs the deletion.
+   * It updates the `root` property if the root node is deleted.
+   */
 
   deleteNode(root: Node | null, value: string): Node | null {
     if (!root) {
@@ -159,27 +203,27 @@ export class BinaryMainComponent implements OnInit {
       return root;
     }
 
-    // If the deleted value is smaller than the root value got to left tree
+    // deleted value is smaller root goes to left tree
     if (value < root.value) {
       root.left = this.deleteNode(root.left, value);
     }
-    // If the deleted value is larger than the root value got to right tree
+    // deleted value is larger root goes to right tree
     else if (value > root.value) {
       root.right = this.deleteNode(root.right, value);
     }
     // If the deleted value is  root, then this is the node to be deleted.
     else {
-      // Case 1: Node to be deleted has no children (leaf node)
+      // Case 1: deleted Node has no children (leaf node)
       if (!root.left && !root.right) {
         root = null;
       }
-      // Case 2: Node to be deleted has only one child
+      // Case 2:deleted Node has only one child
       else if (!root.left) {
         root = root.right;
       } else if (!root.right) {
         root = root.left;
       }
-      // Case 3: Node to be deleted has two children
+      // Case 3:deleted Node has two children
       else {
         // Find the inorder successor (smallest value in the right subtree)
         const minValue = this.findMinValue(root.right);
@@ -218,7 +262,11 @@ export class BinaryMainComponent implements OnInit {
     alert(message);
   }
 
-  //To display Tree dynamically by creating string and render with html class
+  /**
+   * renderTree(node: Node | null)
+   * Recursively generates the HTML content for the binary tree starting from the specified `node`.
+   * It returns the HTML content as a string.
+   */
   renderTree(node: Node | null): any {
     if (!node) {
       return '';
@@ -280,6 +328,12 @@ export class BinaryMainComponent implements OnInit {
     }
   }
 
+  /**
+   * SetMaxLevel()
+   * with comparing the previous maxvalue it setting the current maxvalue for tree to rendered.
+   * and the maxDepath of the tree is 10
+   */
+
   setMaxLevel() {
     const currentLevelOfTree = this.countLevels(this.root);
     if (this.previousMaxLevel !== undefined && this.maxLevel !== undefined) {
@@ -307,6 +361,14 @@ export class BinaryMainComponent implements OnInit {
       this.addEnableFlag = false;
     }
   }
+
+  /**
+   * checkFlag(event:Event)
+   * Checking the flag to set true or false by Tree childern
+   * In the depath if the childern can be stored it can enable 
+   * if the root goes more than maxDeapth then it controls the button
+   * by calling checkButtonFlag
+   */
   checkFlag(event: Event) {
     const checkValue = (event.target as HTMLInputElement).value;
     // this.addEnableFlag = true;
@@ -342,12 +404,14 @@ export class BinaryMainComponent implements OnInit {
     }
   }
 
-  //adding nodes to left or right
+  /**
+   * addNode(root :Node, newNode:Node, level:number =1 )
+   *  
+   *
+   */
   addNode(root: Node, newNode: Node, level: number = 1) {
     if (level >= this.maxLevel) {
-      // console.log('Maximum levels reached. Cannot add more nodes.');
       alert('Reached Max Level');
-      // this.addEnableFlag = false;
       return;
     }
 
@@ -355,7 +419,6 @@ export class BinaryMainComponent implements OnInit {
       if (!root.left) {
         root.left = newNode;
         this.postIntoJson();
-        // console.log('Added node with value', newNode.value, 'to the left of', root.value);
       } else {
         this.addNode(root.left, newNode, level + 1);
       }
@@ -363,14 +426,19 @@ export class BinaryMainComponent implements OnInit {
       if (!root.right) {
         root.right = newNode;
         this.postIntoJson();
-        // console.log('Added node with value', newNode.value, 'to the right of', root.value);
       } else {
         this.addNode(root.right, newNode, level + 1);
       }
     } else {
-      // console.log('Node with value', newNode.value, 'already exists.');
+      alert('Value is already existed')
     }
   }
+
+  /**
+   * postIntoJson()
+   * Retrieves the binary tree data from an API using the Angular `HttpClient` and assigns the response to the `root` property.
+   * If the `root` is `null`, it sets the `rootFlag` to `true`.
+   */
 
   postIntoJson() {
     console.log(this.root, 'data');
@@ -381,7 +449,11 @@ export class BinaryMainComponent implements OnInit {
       });
   }
 
-  //BST Traversals
+  /**
+   * displayTraversals()
+   * Checking the variable with which order needs to be called by passing the argument value
+   * before calling the traversal its making  inOrderTraversal list empty to store the new values
+   */
   displayTraversals() {
     if (this.root) {
       console.log(this.root);
@@ -415,6 +487,10 @@ export class BinaryMainComponent implements OnInit {
   //   }
   // }
 
+  /**
+   * inOrderTraversal(node:Node | null)
+   * Methods which returns the tree in form of inorder practise
+   */
   inorderTraversal(node: Node | null) {
     if (node) {
       this.inorderTraversal(node.left);
@@ -423,6 +499,11 @@ export class BinaryMainComponent implements OnInit {
       this.inorderTraversal(node.right);
     }
   }
+
+  /**
+   * preOrderTraversal(node:Node | null)
+   * Methods which returns the tree in form of preorder practise
+   */
 
   preorderTraversal(node: Node | null) {
     if (node) {
@@ -433,6 +514,10 @@ export class BinaryMainComponent implements OnInit {
     }
   }
 
+  /**
+   * postOrderTraversal(node:Node | null)
+   * Methods which returns the tree in form of postorder practise
+   */
   postorderTraversal(node: Node | null) {
     if (node) {
       this.postorderTraversal(node.left);
@@ -442,11 +527,16 @@ export class BinaryMainComponent implements OnInit {
     }
   }
 
-  //To check the current level of tree
+  //To check the current level of tree by calling the countLevel() function
   checkLevel() {
     console.log('level of tree is : ', this.countLevels(this.root));
   }
-
+  /**
+   * 
+   * countLevels(node:Node |null, level) 
+   * the countLevel is couting the current tree deapth
+   * returns the number
+   */
   countLevels(node: Node | null, level: number = 0): number {
     if (!node) {
       return level;
@@ -458,7 +548,14 @@ export class BinaryMainComponent implements OnInit {
     return Math.max(leftLevel, rightLevel);
   }
 }
-
+/**
+ * defining Node with attributes
+ * {
+ * value:string,
+ * left:Node|null,
+ * right:Node|null,
+ * }
+ */
 class Node {
   constructor(public value: string) {
     this.left = null;
